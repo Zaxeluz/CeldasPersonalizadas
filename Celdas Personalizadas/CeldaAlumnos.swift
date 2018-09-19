59dc00ef4cdffa8ff4a43cfcd75294f5b1f1190f
Alumno <ulsa@iMacD113.local>
2018-09-18 18:53:14 -0700

    Celdas

---
+++ Celdas Personalizadas/CeldaAlumnos.swift
@@ -0,0 +1,16 @@
+//
+//  CeldaAlumnos.swift
+//  Celdas Personalizadas
+//
+//  Created by Alumno on 18/09/18.
+//  Copyright © 2018 Alumno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CeldaAlumnos : UITableViewCell {
+    
+    @IBOutlet weak var lblAlumno: UILabel!
+    @IBOutlet weak var lblPromedio: UILabel!
+}
